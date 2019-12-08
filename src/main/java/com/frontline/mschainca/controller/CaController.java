43cef11709efd4116dbba695e51aa066856c0f65
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-12-08 17:03:18 +0530

    cors support

--- src/main/java/com/frontline/mschainca/controller/CaController.java
+++ src/main/java/com/frontline/mschainca/controller/CaController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -112,6 +109,7 @@ public class CaController {
 //    }
 
     @ResponseBody
+    @CrossOrigin
     @RequestMapping(value = "/certificate/revoke", method = RequestMethod.POST)
     public ResponseEntity<ResponseDto> revokeCertificate(@RequestBody CertificateDto certificateToRevoke) {
         LOGGER.log(Level.INFO, "REQUEST :\n" + certificateToRevoke.toString());
